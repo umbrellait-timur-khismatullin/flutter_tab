fbd10c6e8f14a972889f0c9fdb5c458259db6d6a
timur.khismatullin <timur.khismatullin@umbrellait.com>
2023-03-20 18:41:49 +0300

    Add Flutter fragment

--- app/src/main/java/com/example/fluttertab/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/fluttertab/ui/dashboard/DashboardFragment.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.FragmentManager
+import com.example.fluttertab.R
 import com.example.fluttertab.databinding.FragmentDashboardBinding
+import io.flutter.embedding.android.FlutterFragment
 
 class DashboardFragment : Fragment() {
+
+
+    companion object {
+        // Define a tag String to represent the FlutterFragment within this
+        // Activity's FragmentManager. This value can be whatever you'd like.
+        private const val TAG_FLUTTER_FRAGMENT = "flutter_fragment"
+    }
+
+    // Declare a local variable to reference the FlutterFragment so that you
+    // can forward calls to it later.
+    private var flutterFragment: FlutterFragment? = null
+
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -22,17 +35,33 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val fragmentManager: FragmentManager = parentFragmentManager
+
+        flutterFragment = fragmentManager
+            .findFragmentByTag(TAG_FLUTTER_FRAGMENT) as FlutterFragment?
+
+        // Attempt to find an existing FlutterFragment, in case this is not the
+        // first time that onCreate() was run.
+        flutterFragment = fragmentManager
+            .findFragmentByTag(TAG_FLUTTER_FRAGMENT) as FlutterFragment?
+
+        // Create and attach a FlutterFragment if one does not exist.
+
+            val newFlutterFragment =
+                FlutterFragment.withCachedEngine("my_engine_id").build<FlutterFragment>()
+            flutterFragment = newFlutterFragment
+            fragmentManager
+                .beginTransaction()
+                .add(
+                    R.id.fragmentContainerView,
+                    newFlutterFragment,
+                    TAG_FLUTTER_FRAGMENT
+                )
+                .commit()
+
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
