eef45ae19877e9c479ae5b8199f1fc9504f3e4b8
timur.khismatullin <timur.khismatullin@umbrellait.com>
2023-03-21 09:48:49 +0300

    Refactor DashboardFragment.kt

--- app/src/main/java/com/example/fluttertab/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/fluttertab/ui/dashboard/DashboardFragment.kt
@@ -5,29 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.example.fluttertab.R
 import com.example.fluttertab.databinding.FragmentDashboardBinding
 import io.flutter.embedding.android.FlutterFragment
 
 class DashboardFragment : Fragment() {
 
-
     companion object {
-        // Define a tag String to represent the FlutterFragment within this
-        // Activity's FragmentManager. This value can be whatever you'd like.
         private const val TAG_FLUTTER_FRAGMENT = "flutter_fragment"
     }
 
-    // Declare a local variable to reference the FlutterFragment so that you
-    // can forward calls to it later.
-    private var flutterFragment: FlutterFragment? = null
-
-
     private var _binding: FragmentDashboardBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -35,31 +24,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val fragmentManager: FragmentManager = parentFragmentManager
-
-        flutterFragment = fragmentManager
-            .findFragmentByTag(TAG_FLUTTER_FRAGMENT) as FlutterFragment?
-
-        // Attempt to find an existing FlutterFragment, in case this is not the
-        // first time that onCreate() was run.
-        flutterFragment = fragmentManager
-            .findFragmentByTag(TAG_FLUTTER_FRAGMENT) as FlutterFragment?
-
-        // Create and attach a FlutterFragment if one does not exist.
-
-            val newFlutterFragment =
-                FlutterFragment.withCachedEngine("my_engine_id").build<FlutterFragment>()
-            flutterFragment = newFlutterFragment
-            fragmentManager
-                .beginTransaction()
-                .add(
-                    R.id.fragmentContainerView,
-                    newFlutterFragment,
-                    TAG_FLUTTER_FRAGMENT
-                )
-                .commit()
-
-
+        parentFragmentManager
+            .beginTransaction()
+            .add(
+                R.id.fragmentContainerView,
+                FlutterFragment.withCachedEngine("my_engine_id").build<FlutterFragment>(),
+                TAG_FLUTTER_FRAGMENT
+            )
+            .commit()
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         return binding.root
     }
